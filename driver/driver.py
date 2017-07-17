455f8142a4af29acb2103da055ce02948330cdfa
Urabn <prevcurban@gmail.com>
2017-07-17 19:18:03 +0200

    Cleaned up and commented all code.

--- driver/driver.py
+++ driver/driver.py
@@ -1,14 +1,15 @@
 from __future__ import print_function
-import getopt, sys
-from math import sqrt
-import heapq
-import timeit
-import os
-#import thread
-import itertools
 from collections import deque
+from math import sqrt
+import getopt
+import heapq
+import itertools
+import os
+import sys
+import timeit
 
 def memory():
+    """Returns program's memory usage, Windows or Unix."""
     if os.name == 'posix':
         import resource
         return float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024)
@@ -18,9 +19,11 @@ def memory():
         return float(p.memory_info().rss / 1048576)
 
 class PriorityQueue(object):
+    """Queue with priority based on path and add order."""
     def __init__(self):
         self.elements = []
         self.counter = itertools.count()
+        self.entries = 0
     def isEmpty(self):
         return len(self.elements) == 0
     def put(self, item, priority):
@@ -38,7 +41,7 @@ class PriorityQueue(object):
         self.put(old, item.depth + solver.manhattan_distance(item, goal_state))
 
 class State(object):
-    """ represents a state/ node """
+    """Represents a state/ node."""
     def __init__(self, configuration, path, parent, depth):
         self.configuration = configuration
         self.path = path
@@ -49,32 +52,11 @@ class State(object):
             return self.configuration == other[1].configuration
         else:
             return other and self.configuration == other.configuration
-
-class Queue:
-    def __init__(self):
-        self.items = []
-    def isEmpty(self):
-        return self.items == []
-    def enqueue(self, item):
-        self.items.insert(0,item)
-    def dequeue(self):
-        return self.items.pop()
-    def size(self):
-        return len(self.items)
-
-class Stack:
-     def __init__(self):
-         self.items = []
-     def isEmpty(self):
-         return self.items == []
-     def push(self, item):
-         self.items.append(item)
-     def pop(self):
-         return self.items.pop()
-     def size(self):
-         return len(self.items)
+    def __lt__(self, other):
+        return float(str("%s.%s" % (self.depth, self.path))) < float(str("%s.%s" % (other.depth, other.path)))
 
 class Solver(object):
+    """Handles all solving methods."""
     def __init__(self, initial_state):
         self.n = len(initial_state.configuration) - 1
         self.pow = int(sqrt(self.n + 1))
@@ -89,6 +71,7 @@ class Solver(object):
         self.max_search_depth = 0
         self.path_to_goal = []
     def solvable(self, configuration):
+        """Teests if puzzle is solvable"""
         inversions = 0
         cfg = list(configuration)
         cfg.remove(0)
@@ -99,7 +82,12 @@ class Solver(object):
                     inversions += 1
         return inversions % 2 == 0
     def generate_state(self, current_state, direction):
-        """ generates a new state """
+        """Generates a new state.
+        
+        Attributes:
+            current_state -- state to generate new state from
+            direction -- direction from current state to get to new state
+        """
         current = current_state.configuration.index(0)
         if direction == 1:
             next = int(current - sqrt(self.n + 1))
@@ -115,7 +103,7 @@ class Solver(object):
         next_direction = direction
         return State(next_configuration, next_direction, current_state, current_state.depth + 1)
     def generate_neighbours(self, current_state):
-        """ generates given state's neighbours """
+        """Generates list of given state's neighbours."""
         current_index = current_state.configuration.index(0)
         neighbours = []
         if current_index not in self.top and (current_state.path == "" or not current_state.path == 2):
@@ -128,7 +116,7 @@ class Solver(object):
             neighbours.append(self.generate_state(current_state, 4))
         return neighbours
     def generate_neighbours_RLDU(self, current_state):
-        """ generates given state's neighbours """
+        """Generates given state's neighbours in reverse order."""
         current_index = current_state.configuration.index(0)
         neighbours = []
         if current_index not in self.right and (current_state.path == "" or not current_state.path == 3):
@@ -141,6 +129,7 @@ class Solver(object):
             neighbours.append(self.generate_state(current_state, 1))
         return neighbours
     def generate_path(self, initial_state, current_state):
+        """Genetrates path from initial to current state."""
         directions = ["Up", "Down", "Left", "Right"]
         path_to_goal = []
         while not str(current_state.configuration) == str(initial_state.configuration):
@@ -148,6 +137,7 @@ class Solver(object):
             current_state = current_state.parent
         return path_to_goal
     def manhattan_distance(self, current_state, goal_state):
+        """Returns Manhattan distance from current state to goal."""
         m_distance = 0
         for n in range(self.n + 1):
             where_it_is = current_state.configuration.index(n)
@@ -158,21 +148,19 @@ class Solver(object):
         return int(m_distance)
 
 def bfs(solver, initial_state, goal_test):
+    """Breadth First Search.
+    
+    Frontier is a Queue - FILO"""
     if not solver.solvable(initial_state.configuration):
         return False
-    #frontier = Queue()
     frontier = deque()
-    #frontier.enqueue(initial_state)
     frontier.append(initial_state)
     explored = set()
     explored.add(str(initial_state.configuration))
-    while len(frontier) > 0: # frontier.isEmpty()
-        #solver.max_fringe_size = max(solver.max_fringe_size, frontier.size())
+    while len(frontier) > 0:
         solver.max_fringe_size = max(solver.max_fringe_size, len(frontier))
-        #current_state = frontier.dequeue()
         current_state = frontier.popleft()
         if current_state.configuration == goal_test:
-            #solver.fringe_size = frontier.size()
             solver.fringe_size = len(frontier)
             solver.path_to_goal = solver.generate_path(initial_state, current_state)
             solver.search_depth = len(solver.path_to_goal)
@@ -180,36 +168,41 @@ def bfs(solver, initial_state, goal_test):
         solver.nodes_expanded += 1
         for n in solver.generate_neighbours(current_state):
             if str(n.configuration) not in explored:
-                #frontier.enqueue(n)
                 frontier.append(n)
                 explored.add(str(n.configuration))
                 solver.max_search_depth = max(solver.max_search_depth, n.depth)
     return False
 
 def dfs(solver, initial_state, goal_test):
+    """Depth first search.
+    
+    Frontier is a Stack - LIFO"""
     if not solver.solvable(initial_state.configuration):
         return False
-    frontier = Stack()
-    frontier.push(initial_state)
+    frontier = []
+    frontier.append(initial_state)
     explored = set()
     explored.add(str(initial_state.configuration))
-    while not frontier.isEmpty():
-        solver.max_fringe_size = max(solver.max_fringe_size, frontier.size())
+    while not frontier == []:
+        solver.max_fringe_size = max(solver.max_fringe_size, len(frontier))
         current_state = frontier.pop()
         if current_state.configuration == goal_test:
-            solver.fringe_size = frontier.size()
+            solver.fringe_size = len(frontier)
             solver.path_to_goal = solver.generate_path(initial_state, current_state)
             solver.search_depth = len(solver.path_to_goal)
             return current_state
         solver.nodes_expanded += 1
         for n in solver.generate_neighbours_RLDU(current_state):
             if str(n.configuration) not in explored:
-                frontier.push(n)
+                frontier.append(n)
                 explored.add(str(n.configuration))
                 solver.max_search_depth = max(solver.max_search_depth, n.depth)
     return False
 
 def ast(solver, initial_state, goal_test):
+    """A* search.
+    
+    Frontier is a priority queue"""
     if not solver.solvable(initial_state.configuration):
         return False
     frontier = PriorityQueue()
@@ -235,6 +228,7 @@ def ast(solver, initial_state, goal_test):
     return False
 
 def print_end(sol_solver, method):
+    """Prints details from solve."""
     print(method)
     #print("path_to_goal: " + str(sol_solver.path_to_goal))
     print("cost_of_path: " + str(len(sol_solver.path_to_goal)))
@@ -245,15 +239,19 @@ def print_end(sol_solver, method):
     print("max_search_depth: " + str(sol_solver.max_search_depth))
     print("max_ram_usage " + str(memory()) + "\n")
 
-goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+#goal_state = list(range(0, 9))
+goal_state = list(range(1, 16)) + [0]
 #file = open("output.txt", "w")
-initial_state = State([4, 1, 3, 7, 6, 0, 5, 2, 8], [], [], 0)
+#initial_state = State([1,2,5,3,4,0,6,7,8], [], [], 0)
+#initial_state = State([4, 1, 3, 7, 6, 0, 5, 2, 8], [], [], 0)
+initial_state = State([1,2,8,3,6,0,7,4,5,9,13,15,10,11,12,14], [], [], 0)
+#initial_state = State([8,6,2,14,4,0,15,1,13,12,9,7,3,5,10,11], [], [], 0)
 sol_solver = Solver(initial_state)
 start = timeit.default_timer()
-bfs(sol_solver, initial_state, goal_state)
-print_end(sol_solver, "BFS:")
-dfs(sol_solver, initial_state, goal_state)
-print_end(sol_solver, "DFS:")
+#bfs(sol_solver, initial_state, goal_state)
+#print_end(sol_solver, "BFS:")
+#dfs(sol_solver, initial_state, goal_state)
+#print_end(sol_solver, "DFS:")
 ast(sol_solver, initial_state, goal_state)
 print_end(sol_solver, "AST:")
 stop = timeit.default_timer()
